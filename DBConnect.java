1e2fbafd86c92ed620b03fe554b2cc09b086bda2
tianwei <464231692@qq.com>
2020-07-02 03:17:54 -0400

    generic

--- DBConnect.java
+++ DBConnect.java
@@ -7,74 +7,58 @@ import java.net.*;
 
 public class DBConnect{
     public static void main(String[] args) throws Exception{
-        // Connection con;
-        // Statement st;
-        // ResultSet rs;
-
-        // String url = "jdbc:postgresql://localhost:5432/transnomis";
-        // String user = "postgres";
-        // String password = "651125";
-
-        // try {
-        //     Class.forName("org.postgresql.Driver");
-        // } catch (ClassNotFoundException e) {
-        //     e.printStackTrace();
-        // }
-
-        // con = DriverManager.getConnection(url,user,password);
-        // st = con.createStatement();
-
-        // rs = st.executeQuery("select date,hour from observation");
-        // while(rs.next()){
-        //     System.out.print(rs.getString("date")+",");
-        //     System.out.println(rs.getString("hour"));
-        // }
-        // readCSV();
-
-        // rs.close();
-        // st.close();
-        // con.close();
+        
 
 
         String url = "https://dd.weather.gc.ca/air_quality/aqhi/ont/observation/realtime/csv/2020070200_AQHI_ON_SiteObs.csv";
 
-        try {
+        
 
-            Timer time = new Timer(false);
-            time.schedule(new TimerTask(){
+        Timer time = new Timer(false);
+
+        time.schedule(new TimerTask(){
                 public void run(){
-                    downloadUsingStream(url, "./ontario.csv");
+                    // downloadUsingStream(url, "./ontario.csv");
+                    System.out.println(new java.util.Date());
                 }
-            },new java.util.Date(),2000);
+        },new java.util.Date(),1000*60*60);
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        // readCSV();
+
+        PGConnection();
 
 
 
     }
 
+    
 
-    public static List readCSV() {
+    public static List<KGInfo> readCSV() {
         try {
-            ArrayList<String> list = new ArrayList<String>();
-            BufferedReader reader = new BufferedReader(new FileReader("./ON_sample.csv"));
-            // reader.readLine();
+            List<KGInfo> kgInfoList = new ArrayList<>();
+            BufferedReader reader = new BufferedReader(new FileReader("./ontario.csv"));
+            reader.readLine();
             String line = null;
             while ((line = reader.readLine()) != null) {
+                KGInfo kgInfo=new KGInfo();
                 String item[] = line.split(",");
-                String last = item[item.length - 1];
+                kgInfo.setDate(item[0]);
+                kgInfo.setHour(item[1]);
+                kgInfo.setFaffd(item[2]);
+
+                kgInfoList.add(kgInfo);
+                
                 // int value = Integer.parseInt(last);
 
-                list.add(last);
+                // list.add(last);
                 // System.out.println(last);
             }
-            System.out.println("data of the line：" + list);
-            return list;
+            System.out.println(list);
+            
         } catch (Exception e) {
         }
-        return null;
+        return kgInfoList;
         
     }
 
@@ -93,5 +77,55 @@ public class DBConnect{
     }
 
 
+    public static void PGConnection()  throws Exception{
+        Connection con;
+        Statement st;
+        ResultSet rs;
 
+        String url = "jdbc:postgresql://localhost:5432/transnomis";
+        String user = "postgres";
+        String password = "651125";
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        List list = readCSV();
+
+        con = DriverManager.getConnection(url,user,password);
+        con.setAutoCommit(false);
+        PreparedStatement prep = conn.prepareStatement("INSERT INTO observation (date,hour,faffd)  VALUES (?,?,?)");
+        int num=0;
+        for (KGInfo value : list) {
+                num++;
+                prep.setString(1, value.getDate());
+                prep.setInteger(2,value.getHour());
+                prep.setDouble(3,value.getFaffd());
+                prep.addBatch();
+                if(num>100){
+                    System.out.println(prep);
+                    prep.executeBatch();
+                    conn.commit();
+                    num=0;
+                }
+                System.out.println(prep);
+                prep.executeBatch();
+                conn.commit();
+        }
+
+
+        rs.close();
+        st.close();
+        con.close();
+    }
+
+
+}
+
+
+
+class KGInfo{
+    
 }
